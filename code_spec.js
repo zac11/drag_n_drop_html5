a3d388e1b2bc13c9fc574ba3bebaaf6fd661d115
zac11 <ydv.rahul09@gmail.com>
2017-07-06 15:52:21 +0530

    "Added code for Angular"

--- code_spec.js
+++ code_spec.js
@@ -9,7 +9,7 @@ describe('Dragging and dropping', ()=> {
          var elementtoDrop = element(by.css('a#one'));
          var elementWhereToDrop = element(by.css('div#bin'));
 
-         //now it should drag and drop 
+         //now it should drag and drop - fails
          browser.executeScript(helperfiles,elementtoDrop.getWebElement(),elementWhereToDrop.getWebElement());
          
          browser.sleep(5000);
@@ -26,7 +26,7 @@ describe('Dragging and dropping for second test', ()=> {
         browser.sleep(5000);
         var elementtoDrop = element(by.css('[data-drag="true"]')).click();
         var elementWhereToDrop = element(by.css('[data-drop="true"]'));
-        //now it should do drag and drop
+        //now it should do drag and drop - fails
         browser.executeScript(helperfiles,elementtoDrop.getWebElement(),elementWhereToDrop.getWebElement());
 
         browser.sleep(5000);
@@ -35,7 +35,7 @@ describe('Dragging and dropping for second test', ()=> {
 });
 
 
-describe('Dragging and dropping for second test', ()=> {
+describe('Dragging and dropping for third test', ()=> {
         browser.ignoreSynchronization=true;
 
     it('takes an elements and performs drag and drop', ()=> {
@@ -43,12 +43,32 @@ describe('Dragging and dropping for second test', ()=> {
         browser.sleep(5000);
         var elementtoDrop = element(by.css('img#drag1'));
         var elementWhereToDrop = element(by.css('div#div2'));
-        //now it should do drag and drop
+        //now it should do drag and drop - succeeds
         browser.executeScript(helperfiles,elementtoDrop.getWebElement(),elementWhereToDrop.getWebElement());
 
         browser.sleep(5000);
     });
         
 });
+
+
+describe('Dragging and dropping for Angular 1 application', function() {
+        browser.ignoreSynchronization=true;
+
+    it('takes an elements and performs drag and drop', ()=> {
+        browser.get('https://marceljuenemann.github.io/angular-drag-and-drop-lists/demo/#/nested');
+        browser.sleep(5000);
+        var elementtoDrop = element(by.xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/div/div/ul/li[1]/div/div[1]/ul/li[1]/div')).click();
+        var elementWhereToDrop = element(by.xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/div/div/ul/li[4]/div'));
+        //now it should do drag and drop - fails
+        browser.executeScript(helperfiles,elementtoDrop.getWebElement(),elementWhereToDrop.getWebElement());
+        browser.sleep(5000);
+    });
+        
+    
+});
+
+
+    
     
     
